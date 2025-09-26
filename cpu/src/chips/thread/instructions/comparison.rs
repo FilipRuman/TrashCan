e9e6c17967c0954db8e2db23fb8dbfa438c76afe
FR <137614590+FilipRuman@users.noreply.github.com>
2025-09-26 20:08:23 +0200

    made boolean logic operations return all bites set to value

--- cpu/src/chips/thread/instructions/comparison.rs
+++ cpu/src/chips/thread/instructions/comparison.rs
@@ -6,41 +6,51 @@ use crate::{
 };
 impl Thread {
     pub fn Eq(&self, a_register: B8, b_register: B8, out_register: B8, run: bool) {
+        let operation_output =
+            (self.registers.read(a_register) == self.registers.read(b_register)) as u32;
+        // make all bits have value of first bit
         self.registers.write(
-            B32((self.registers.read(a_register) == self.registers.read(b_register)) as u32),
+            B32((operation_output & 1).wrapping_neg()),
             out_register,
             run,
         );
     }
     pub fn Gte(&self, a_register: B8, b_register: B8, out_register: B8, run: bool) {
+        let operation_output =
+            (self.registers.read(a_register) >= self.registers.read(b_register)) as u32;
+        // make all bits have value of first bit
         self.registers.write(
-            B32((self.registers.read(a_register) >= self.registers.read(b_register)) as u32),
+            B32((operation_output & 1).wrapping_neg()),
             out_register,
             run,
         );
     }
     pub fn Lte(&self, a_register: B8, b_register: B8, out_register: B8, run: bool) {
+        let operation_output =
+            (self.registers.read(a_register) <= self.registers.read(b_register)) as u32;
+        // make all bits have value of first bit
         self.registers.write(
-            B32((self.registers.read(a_register) <= self.registers.read(b_register)) as u32),
+            B32((operation_output & 1).wrapping_neg()),
             out_register,
             run,
         );
     }
     pub fn Lt(&self, a_register: B8, b_register: B8, out_register: B8, run: bool) {
+        let operation_output =
+            (self.registers.read(a_register) < self.registers.read(b_register)) as u32;
+        // make all bits have value of first bit
         self.registers.write(
-            B32((self.registers.read(a_register) < self.registers.read(b_register)) as u32),
+            B32((operation_output & 1).wrapping_neg()),
             out_register,
             run,
         );
     }
     pub fn Gt(&self, a_register: B8, b_register: B8, out_register: B8, run: bool) {
-        info!(
-            "Gt a-r{a_register}: {} b-r{b_register}: {}",
-            self.registers.read(a_register),
-            self.registers.read(b_register)
-        );
+        let operation_output =
+            (self.registers.read(a_register) > self.registers.read(b_register)) as u32;
+        // make all bits have value of first bit
         self.registers.write(
-            B32((self.registers.read(a_register) > self.registers.read(b_register)) as u32),
+            B32((operation_output & 1).wrapping_neg()),
             out_register,
             run,
         );
